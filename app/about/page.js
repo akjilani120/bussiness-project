5a50973d006d80db445dace051e207a82fbb39a4
akjilani120 <abdulkaderjilani120@gmail.com>
2023-10-25 14:11:20 +0600

    added some

--- app/about/page.js
+++ app/about/page.js
@@ -132,7 +132,7 @@ const AboutPage = () => {
     window.addEventListener("scroll", handleSlideSec);
     window.addEventListener("scroll", handleParshSec);
     window.addEventListener("scroll", handleLight);
-  }, []);
+  });
 
   const images = [
     "https://images.unsplash.com/photo-1579508542697-bb18e7d9aeaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
